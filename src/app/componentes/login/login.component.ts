f5682768d77da54dfb6526d3a779fe31ba018c1b
Fernando <FernandoNieto71>
2022-06-27 13:15:01 -0300

    usuario test

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -7,78 +7,89 @@ import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  miUsuario:Usuario;
-  existe:boolean;
-  leyendaNoExiste:string;
-  claveErronea:boolean;
-  constructor(private router: Router) { 
-  
-    this.miUsuario=new Usuario();
-    this.existe=false;
-    this.leyendaNoExiste='Ususario no existe';
+  miUsuario: Usuario;
+  existe: boolean;
+  leyendaNoExiste: string;
+  claveErronea: boolean;
+  textoUsuario: string;
+  textoClave: string;
+  constructor(private router: Router) {
+    this.miUsuario = new Usuario();
+    this.existe = false;
+    this.leyendaNoExiste = 'Ususario no existe';
     this.claveErronea = false;
-
+    this.textoClave = 'Password';
+    this.textoUsuario = 'Email address';
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  mostrarDatos(): void {
+    console.log('texto ' + this.miUsuario.nombre);
+    console.info('objeto', this.miUsuario);
+    if (this.miUsuario.nombre == 'admin' && this.miUsuario.clave == '1234') {
+      this.router.navigate(['bienvenidos']);
+    } else {
+      this.router.navigate(['registro']);
+    }
   }
 
-mostrarDatos(): void
-{
-  console.log("texto " + this.miUsuario.nombre);
-  console.info("objeto", this.miUsuario);
-  if (this.miUsuario.nombre == "admin" && this.miUsuario.clave == "1234"){
-    this.router.navigate(["bienvenidos"]);
-  }
-   else {
-     this.router.navigate(["registro"]);
-   } 
-  }
-
-  validaLogin():void{
-    if(localStorage.getItem("users")!=null){
-      var users = JSON.parse(localStorage.getItem('users'))
+  validaLogin(): void {
+    if (localStorage.getItem('users') != null) {
+      var users = JSON.parse(localStorage.getItem('users'));
     }
     let encontrado = 0;
-    for(let i in users ){
+    for (let i in users) {
       //window.alert(users[i]["mail"]+ " " +this.miUsuario.nombre + " "+ this.miUsuario.clave);
-      if(users[i]["mail"] == this.miUsuario.mail){
+      if (users[i]['mail'] == this.miUsuario.mail) {
         encontrado = 1;
-        if(users[i]["clave"] == this.miUsuario.clave){
+        if (users[i]['clave'] == this.miUsuario.clave) {
           //window.alert("Bienvenido " + users[i]["nombre"]);
           this.miUsuario.guardaJugador();
-          this.router.navigate(["juegos"]);
+          this.router.navigate(['juegos']);
 
           break;
-        }
-        else{
+        } else {
           //window.alert("Acceso no Concedido");
           this.claveErronea = true;
         }
       }
     }
-    if(!encontrado){
+    if (!encontrado) {
       //window.alert("Usuario no existe");
-      this.existe=true;
+      this.existe = true;
 
       //this.router.navigate(["registro"]);
     }
-
-    
-
   }
 
-  reIntentar():void{
-    this.existe= false;
-    this.claveErronea= false;
+  reIntentar(): void {
+    this.existe = false;
+    this.claveErronea = false;
   }
 
-  irRegistro():void{
-    this.router.navigate(["registro"]);
+  irRegistro(): void {
+    this.router.navigate(['registro']);
+  }
+
+  usuarioTest() {
+    this.textoClave = '1234';
+    this.textoUsuario = 'tester';
+    this.miUsuario.nombre = 'tester';
+    this.miUsuario.mail = 'test';
+    this.miUsuario.clave = '1234';
+    var users = JSON.parse(localStorage.getItem('users'));
+    let verificar = true;
+    for (let i in users) {
+      if (users[i]['mail'] == 'test') {
+        verificar = false;
+      }
+    }
+    if (verificar) {
+      this.miUsuario.registra();
+    }
   }
 }
-
-
