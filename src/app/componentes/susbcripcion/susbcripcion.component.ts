b970bf8fe6cfcee4d556ce61565f0445553fe986
Fernando <FernandoNieto71>
2022-07-02 20:37:57 -0300

    parcial 1.1

--- src/app/componentes/susbcripcion/susbcripcion.component.ts
+++ src/app/componentes/susbcripcion/susbcripcion.component.ts
@@ -3,54 +3,61 @@ import { Usuario } from 'src/app/clases/usuario';
 import { SuscripcionService } from 'src/app/servicio/suscripcion.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-susbcripcion',
   templateUrl: './susbcripcion.component.html',
-  styleUrls: ['./susbcripcion.component.css']
+  styleUrls: ['./susbcripcion.component.css'],
 })
 export class SusbcripcionComponent implements OnInit {
   jugadorActual: Usuario;
-  listaSuscripciones= [];
+  listaSuscripciones = [];
+  packbusqueda: boolean;
+  tipoBusqueda:any;
 
-  constructor(private servicio: SuscripcionService, private router: Router) { 
+  constructor(private servicio: SuscripcionService, private router: Router) {
     this.jugadorActual = new Usuario();
     this.jugadorActual.traeUsuario();
-
+    this.packbusqueda = false;
     /*this.listaSuscripciones=[];
     if (localStorage.getItem('suscripciones') != null) {
       this.listaSuscripciones = JSON.parse(localStorage.getItem('suscripciones'));
       //console.log(JSON.parse(this.listaSuscripciones[0]));
     }*/
-    this.listaSuscripciones=this.servicio.get('suscripciones')
+    this.listaSuscripciones = this.servicio.get('suscripciones');
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  tateti():void{
-    this.servicio.accesoPack2()
-    this.router.navigate(["tateti"]);
-  }
-
-  ppt():void{
-    this.servicio.accesoPack3();
-    this.router.navigate(["ppt"]);
-  }  
-
-  adivina():void{
+  tateti(): void {
     this.servicio.accesoPack2();
-    this.router.navigate(["adivina"]);
+    this.router.navigate(['tateti']);
   }
 
-  ahorcado():void{
+  ppt(): void {
     this.servicio.accesoPack3();
-    this.router.navigate(["ahorcado"]);
+    this.router.navigate(['ppt']);
   }
 
-  mayMenor():void{
-    this.servicio.accesoPack1();
-    this.router.navigate(["mayormenor"]);
-  }  
+  adivina(): void {
+    this.servicio.accesoPack2();
+    this.router.navigate(['adivina']);
+  }
 
+  ahorcado(): void {
+    this.servicio.accesoPack3();
+    this.router.navigate(['ahorcado']);
+  }
+
+  mayMenor(): void {
+    this.servicio.accesoPack1();
+    this.router.navigate(['mayormenor']);
+  }
+
+  buscaPack(tipo: any) {
+    this.packbusqueda = true;
+    this.tipoBusqueda=tipo;
+  }
+  buscaUsuario() {
+    this.packbusqueda = false;
+  }
 }
